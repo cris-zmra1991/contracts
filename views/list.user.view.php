56110be749b7d20ad315dfe6a48ce50bfbf89f2e
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-03-06 16:41:33 -0500

    cambio de titulo

--- views/list.user.view.php
+++ views/list.user.view.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Listado de contratos</title>
 	<link rel="stylesheet" href="res/panel.css">
 	<link rel="stylesheet" href="res/fontawesome/css/fontawesome.min.css">
 	<link rel="stylesheet" href="res/fontawesome/css/solid.min.css">
