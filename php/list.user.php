9c8cbd84aa59924d929271bae85f0336b266e7c5
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-03-06 12:27:44 -0500

    Agregado php para gestion de pagina de listado de contratos

---
+++ php/list.user.php
@@ -0,0 +1,3 @@
+<?php 
+require '../views/list.user.view.php';
+?>
---
+++ views/list.user.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="res/panel.css">
+	<link rel="stylesheet" href="res/fontawesome/css/fontawesome.min.css">
+	<link rel="stylesheet" href="res/fontawesome/css/solid.min.css">
+</head>
+<body>
+	<div class="main-nav">
+		<div class="first-group">
+			<span class="icon"><i class="fa-solid fa-edit fa-lg"></i></span> 
+			<div class="menu-item"><a href="#">Inicio</a></div>
+			<div class="menu-item"><a href="#">Listado</a></div>
+			<div class="menu-item"><a href="#">Añadir</a></div>
+			<div class="menu-item"><a href="#">Actualizar</a></div>
+			<div class="menu-item"><a href="#">Nomencladores</a></div>
+		</div>
+		<div class="first-group">
+			<div class="menu-item"><a href="http://localhost:8030/Contracts/config/close.php"><i class="fa-solid fa-sign-out"></i></a></div>
+		</div>
+	</div>
+
+</body>
+</html>
